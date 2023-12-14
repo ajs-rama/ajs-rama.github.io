0513093157e23efb0524868e0d6f887e58bcb03e
ajs-rama <anjasrama604@gmail.com>
2023-12-14 10:51:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle Class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// klik di luar saibar untuk mwnghilangkan saitbar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
